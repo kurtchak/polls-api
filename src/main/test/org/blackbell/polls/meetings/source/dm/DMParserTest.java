1172c128263dd0568aeff4c465dff5b0ff9a4ecb
Ján Korčák <kurtcha@gmail.com>
2018-03-11 02:39:16 +0100

    test added

--- src/main/test/org/blackbell/polls/meetings/source/dm/DMParserTest.java
+++ src/main/test/org/blackbell/polls/meetings/source/dm/DMParserTest.java
@@ -1,5 +1,6 @@
 package org.blackbell.polls.meetings.source.dm;
 
+import junit.framework.Assert;
 import org.blackbell.polls.common.PollDateUtils;
 import org.blackbell.polls.meetings.model.Institution;
 import org.blackbell.polls.meetings.model.Season;
@@ -7,6 +8,7 @@ import org.blackbell.polls.meetings.model.Town;
 import org.blackbell.polls.meetings.source.dm.api.response.DMMeetingsResponse;
 import org.blackbell.polls.meetings.source.dm.dto.MeetingDTO;
 import org.blackbell.polls.meetings.source.dm.dto.SeasonDTO;
+import org.blackbell.polls.meetings.source.dm.dto.SeasonMeetingDTO;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -20,32 +22,32 @@ public class DMParserTest {
 
     private static Town presov;
     private static DMMeetingsResponse meetingsResponse;
-
+/*
     @BeforeClass
     public static void init() {
         meetingsResponse = new DMMeetingsResponse();
 
-        List<SeasonDTO> seasonsDTO = new ArrayList<>();
-        SeasonDTO season0610DTO = new SeasonDTO();
+        List<SeasonMeetingDTO> seasonsDTO = new ArrayList<>();
+        SeasonMeetingDTO season0610DTO = new SeasonMeetingDTO();
         season0610DTO.setName("2006-2010");
-        List<MeetingDTO> meetings0610DTO = new ArrayList<>();
+        List<SeasonMeetingDTO> meetings0610DTO = new ArrayList<>();
 
-        MeetingDTO meeting1DTO = new MeetingDTO();
+        SeasonMeetingDTO meeting1DTO = new SeasonMeetingDTO();
         meeting1DTO.setName("Meeting 1");
         meeting1DTO.setDate("2004-05-03");
         meeting1DTO.setId("1");
         meetings0610DTO.add(meeting1DTO);
 
-        MeetingDTO meeting2DTO = new MeetingDTO();
+        SeasonMeetingDTO meeting2DTO = new SeasonMeetingDTO();
         meeting2DTO.setName("Meeting 2");
         meeting2DTO.setDate("2005-02-27");
         meeting2DTO.setId("2");
         meetings0610DTO.add(meeting2DTO);
 
-        season0610DTO.setMeetingDTOs(meetings0610DTO);
+//        season0610DTO.setMeetingDTOs(meetings0610DTO);
         seasonsDTO.add(season0610DTO);
 
-        SeasonDTO season1014DTO = new SeasonDTO();
+        SeasonMeetingDTO season1014DTO = new SeasonMeetingDTO();
         season1014DTO.setName("2010-2014");
         List<MeetingDTO> meetings1014DTO = new ArrayList<>();
 
@@ -55,10 +57,10 @@ public class DMParserTest {
         meeting3DTO.setId("3");
         meetings1014DTO.add(meeting3DTO);
 
-        season1014DTO.setMeetingDTOs(meetings1014DTO);
+//        season1014DTO.setMeetingDTOs(meetings1014DTO);
         seasonsDTO.add(season1014DTO);
 
-        meetingsResponse.setSeasonDTOs(seasonsDTO);
+//        meetingsResponse.setSeasonDTOs(seasonsDTO);
 
         presov = new Town();
         presov.setName("Prešov");
@@ -67,7 +69,7 @@ public class DMParserTest {
 
     @Test
     public void testParseSeasons() throws Exception {
-        DMParser.parseSeasons(presov, Institution.ZASTUPITELSTVO, meetingsResponse.getSeasonDTOs());
+//        DMParser.parseSeasons(presov, Institution.ZASTUPITELSTVO, meetingsResponse.getSeasonDTOs());
 
         List<Season> seasons = presov.getSeasons();
         assert seasons != null;
@@ -78,21 +80,33 @@ public class DMParserTest {
 
         assert seasons.get(0).getMeetings().get(0).getExtId().equals("1");
         assert seasons.get(0).getMeetings().get(0).getName().equals("Meeting 1");
-        assert seasons.get(0).getMeetings().get(0).getDate().equals(PollDateUtils.parseSimpleDate("2004-05-03"));
+        assert seasons.get(0).getMeetings().get(0).getDate().equals(PollsUtils.parseSimpleDate("2004-05-03"));
 
         assert seasons.get(0).getMeetings().get(1).getExtId().equals("2");
         assert seasons.get(0).getMeetings().get(1).getName().equals("Meeting 2");
-        assert seasons.get(0).getMeetings().get(1).getDate().equals(PollDateUtils.parseSimpleDate("2005-02-27"));
+        assert seasons.get(0).getMeetings().get(1).getDate().equals(PollsUtils.parseSimpleDate("2005-02-27"));
 
         assert seasons.get(1).getName().equals("2010-2014");
         assert seasons.get(1).getInstitution().equals(Institution.ZASTUPITELSTVO);
 
         assert seasons.get(1).getMeetings().get(0).getExtId().equals("3");
         assert seasons.get(1).getMeetings().get(0).getName().equals("Meeting 3");
-        assert seasons.get(1).getMeetings().get(0).getDate().equals(PollDateUtils.parseSimpleDate("2012-11-09"));
+        assert seasons.get(1).getMeetings().get(0).getDate().equals(PollsUtils.parseSimpleDate("2012-11-09"));
+    }
+*/
+    @Test
+    public void testGetSimpleName() throws Exception {
+        String name1 = "doc. PhDr. Štefánia MPH Andraščíková PhD.";
+        String name2 = "RNDr. Zuzana Bednárová PhD.";
+        String name3 = "PhDr. Rudolf Dupkala PhD., MBA, DBA";
+
+        Assert.assertEquals("Štefánia Andraščíková", DMParser.getSimpleName(name1));
+        Assert.assertEquals("Zuzana Bednárová", DMParser.getSimpleName(name2));
+        Assert.assertEquals("Rudolf Dupkala", DMParser.getSimpleName(name3));
     }
 
-    @Test
+
+        @Test
     public void testParseMeetingResponse() throws Exception {
 
     }
